dab4b3ba2bc55424234cab01b985c4cdc630b1ad
Matt Todd <chiology@gmail.com>
2008-04-22 19:38:24 -0400

    Updated specs to test validity of new Post models.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,6 +2,9 @@ require File.join( File.dirname(__FILE__), "..", "spec_helper" )
 
 describe Post do
 
-  it "should have specs"
+  it "should be valid when new" do
+    post = Post.new
+    post.should be_valid
+  end
 
 end
