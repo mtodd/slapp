f17f15514c34d9fd7f690be043b5d8cde5a99178
Matt Todd <chiology@gmail.com>
2008-04-22 20:33:42 -0400

    Post models now require at least some body before being saved; updated specs to match changes.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  
+  validates_length_of :body, :minimum => 2
+  
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,9 +2,15 @@ require File.join( File.dirname(__FILE__), "..", "spec_helper" )
 
 describe Post do
 
-  it "should be valid when new" do
+  it "should NOT be valid when new" do
     post = Post.new
-    post.should be_valid
+    post.should_not be_valid
+  end
+  
+  it "should require at least two body characters to be valid" do
+    post = Post.new
+    post.should_not be_valid
+    post.errors.on(:body).should include("is too short (minimum is 2 characters)")
   end
 
 end
